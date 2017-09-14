d7516452f68ff9e346ef13201c1a9b3e6d7e8d35
Evan Miller <evanmillerny@gmail.com>
2017-09-14 14:45:03 -0600

    add sort

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -9,6 +9,7 @@ class ItemList extends Component {
       this.state = {
         items: null
       }
+      this.handleSortChange = this.handleSortChange.bind(this)
   }
 
 componentDidMount() {
@@ -22,19 +23,50 @@ componentDidMount() {
     })
 }
 
+handleSortChange(e) {
+  const target = e.target.value;
+
+  if (target === 'acending'){
+    this.sortItems(-1, 1)
+  } else if (target === 'decending') {
+    this.sortItems(1, -1)
+  }
+}
+
+sortItems(num1, num2) {
+  const sorted = this.state.items.sort((a, b) => {
+    let nameA = a.name.toLowerCase() 
+    let nameB = b.name.toLowerCase()
+
+    if (nameA < nameB) {
+      return num1;
+    }
+    if (nameA > nameB) {
+      return num2;
+    }
+    return 0;
+  });
+  this.setState({ items: sorted })
+}
+
 render() {
   if (!this.state.items) {
     return ( <div>
               <AddItem />
              </div> )
     } else {
-      
       const item = this.state.items.map(item => {
         return <Item items={item} key={Math.round(Date.now() * Math.random())} />
     })
     return (
       <div>
         <h1>ITEM LSIT</h1>
+        <select id="sort" 
+                name="sort"
+                onChange={this.handleSortChange}>
+          <option>acending</option>
+          <option>decending</option>
+        </select>
         <AddItem />
         {item}
       </div>  

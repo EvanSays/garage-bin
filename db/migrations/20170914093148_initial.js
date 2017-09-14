ef44fb82f44d60410f0daa43762d963d317c9718
Evan Miller <evanmillerny@gmail.com>
2017-09-14 09:38:58 -0600

    add pg database

---
+++ db/migrations/20170914093148_initial.js
@@ -0,0 +1,19 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('items', function (table) {
+      table.increments('id').primary();
+      table.string('name', 64);
+      table.string('reason', 64);
+      table.enum('cleanliness', ['sparkling', 'dusty', 'rancid']);
+
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('items'),
+  ]);
+};
---
+++ knexfile.js
@@ -0,0 +1,34 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/garage_bin',
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: {
+      directory: './db/migrations',
+    },
+    useNullAsDefault: true,
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garage_bin',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+    },
+  },
+};
